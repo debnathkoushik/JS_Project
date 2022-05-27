ce27307abda546a252a9a844683192cc27024638
Koushik Debnath <debnathkoushik605@gmail.com>
2022-05-27 12:46:04 +0530

    non-working code

--- cashRegisterCode/cashRegsiter.js
+++ cashRegisterCode/cashRegsiter.js
@@ -14,14 +14,9 @@ function checkCashRegister(price, cash, cid)
 
     const noteValue = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
     let change = cash - price
+    let noteValueLength = noteValue.length
 
-    if (totalCid < change)
-    {
-        object.status = "INSUFFICIENT_FUNDS"
-        return object
-    }
-
-    else if (totalCid == change)
+    if (totalCid === change)
     {
         object.status = "CLOSED"
         for (let i = 0; i < cid.length; i++)
@@ -32,24 +27,40 @@ function checkCashRegister(price, cash, cid)
         return object
     }
     
-    else if (change < totalCid)
+    else
     { 
         for (let i = 0; i < noteValue.length; i++)
         {    
+            let sum = 0
+
             while (change >= noteValue[i])
             {
                 change = change - noteValue[i]
-                object.change
+                object.change[i] = cid[noteValueLength]
+                sum = sum + noteValue
+                object.change[i][1] = sum
             }
+             noteValueLength--
         }
 
-        object.status = "OPEN"
+        if (change === 0)
+        {
+          object.status = "OPEN"
         
-        return object
+          return object
+        }
+
+        else
+        {
+          object.status = "INSUFFICIENT_FUNDS"
+          object.change = null
+
+          return object
+        }
     }
 }
 
-console.log( checkCashRegister(19.5, 20, [
+checkCashRegister(19.5, 20, [
 	['PENNY', 1.01],
 	['NICKEL', 2.05],
 	['DIME', 3.1],
@@ -59,40 +70,40 @@ console.log( checkCashRegister(19.5, 20, [
 	['TEN', 20],
 	['TWENTY', 60],
 	['ONE HUNDRED', 100]
-]) )
+])
 
-// // console.log(checkCashRegister(19.5, 20, [
-// // 	['PENNY', 0.01],
-// // 	['NICKEL', 0],
-// // 	['DIME', 0],
-// // 	['QUARTER', 0],
-// // 	['ONE', 0],
-// // 	['FIVE', 0],
-// // 	['TEN', 0],
-// // 	['TWENTY', 0],
-// // 	['ONE HUNDRED', 0]
-// // ]))
+console.log(checkCashRegister(19.5, 20, [
+	['PENNY', 0.01],
+	['NICKEL', 0],
+	['DIME', 0],
+	['QUARTER', 0],
+	['ONE', 0],
+	['FIVE', 0],
+	['TEN', 0],
+	['TWENTY', 0],
+	['ONE HUNDRED', 0]
+]))
 
-// // console.log(checkCashRegister(19.5, 20, [
-// // 	['PENNY', 0.01],
-// // 	['NICKEL', 0],
-// // 	['DIME', 0],
-// // 	['QUARTER', 0],
-// // 	['ONE', 1],
-// // 	['FIVE', 0],
-// // 	['TEN', 0],
-// // 	['TWENTY', 0],
-// // 	['ONE HUNDRED', 0]
-// // ]))
+console.log(checkCashRegister(19.5, 20, [
+	['PENNY', 0.01],
+	['NICKEL', 0],
+	['DIME', 0],
+	['QUARTER', 0],
+	['ONE', 1],
+	['FIVE', 0],
+	['TEN', 0],
+	['TWENTY', 0],
+	['ONE HUNDRED', 0]
+]))
 
-// console.log(checkCashRegister(19.5, 20, [
-// 	['PENNY', 1.01],
-// 	['NICKEL', 2.05],
-// 	['DIME', 3.1],
-// 	['QUARTER', 4.25],
-// 	['ONE', 90],
-// 	['FIVE', 55],
-// 	['TEN', 20],
-// 	['TWENTY', 60],
-// 	['ONE HUNDRED', 100]
-// ]))
+console.log(checkCashRegister(19.5, 20, [
+	['PENNY', 1.01],
+	['NICKEL', 2.05],
+	['DIME', 3.1],
+	['QUARTER', 4.25],
+	['ONE', 90],
+	['FIVE', 55],
+	['TEN', 20],
+	['TWENTY', 60],
+	['ONE HUNDRED', 100]
+]))
